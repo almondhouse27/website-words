ba71c6af2820511e5a2a60d7170caa5abd15ff3b
almondhouse27 <dblessent.techguides@gmail.com>
2024-10-27 13:22:29 -0500

    add site-data output; add sort function

--- src/scraper.py
+++ src/scraper.py
@@ -1,14 +1,18 @@
 from utils import setupProjectStructure, \
                   readDataInput, \
-                  writeDataOutput
+                  writeWordData, \
+                  writeSiteData, \
+                  sortDataOutput
 from bs4 import BeautifulSoup
 import logging
 import requests
+
 HEADERS = {'User-Agent': 'Mozilla/5.0'}
 DIRECTORIES = ['data', 'data/input', 'data/output']
 LOG_FILE = 'logs/scraper.log'
 INPUT_FILE = 'data/input/url-list.csv'
 BACKUP_FILE = 'backup/url-list.csv'
+
 logging.basicConfig(
         filename=LOG_FILE,
         level=logging.INFO, 
@@ -26,15 +30,35 @@ def scrapeWebsite(url):
         logging.info(f"Successfully fetched {url}")
 
         soup = BeautifulSoup(response.text, 'lxml')
+
+        # data for word-data-timestamp.csv
         text_content = soup.get_text(separator=' ')
         word_count = {}
-
         for word in text_content.split():
             word = word.lower().strip('.,!?')
             word_count[word] = word_count.get(word, 0) + 1
         logging.info(f"Word count completed for {url}")
 
-        return word_count
+        # data for site-data-timestamp.csv
+        image_count = len(soup.find_all('img'))
+        link_count = len(soup.find_all('a'))
+        js_true = any(script for script in soup.find_all('script'))
+            # placeholder for certificate information
+        cert_info = 'N/A'
+        host_info = 'N/A'
+
+        site_details = {
+            'images': image_count,
+            'links': link_count,
+            'js': js_true,
+            'cert': cert_info,
+            'host': host_info
+        }
+
+        return {
+            'words': word_count,
+            'site': site_details
+        }
     
     except requests.exceptions.RequestException as e:
         logging.error(f"Error fetching {url}: {e}")
@@ -46,15 +70,27 @@ def scrapeWebsite(url):
 """
 if __name__ == "__main__":
 
-    setupProjectStructure(INPUT_FILE, BACKUP_FILE, DIRECTORIES)
+    try:
+        setupProjectStructure(INPUT_FILE, BACKUP_FILE, DIRECTORIES)
 
-    urls_to_scrape = readDataInput()
-    all_word_data = {}
+        urls_to_scrape = readDataInput()
+        all_word_data = {}
+        all_site_data = {}
 
-    for institution, url in urls_to_scrape.items():
-        word_data = scrapeWebsite(url)
+        for institution, url in urls_to_scrape.items():
+            scraped_data = scrapeWebsite(url)
 
-        if word_data:
-            all_word_data[institution] = word_data
+            if scraped_data:
+                all_word_data[institution] = scraped_data['words']
+                all_site_data[institution] = scraped_data['site']
 
-    writeDataOutput(all_word_data)
+        writeWordData(all_word_data)
+        writeSiteData(all_site_data)
+
+        sortDataOutput(
+            'data/output/word-data-*.csv', 
+            'data/output/site-data-*.csv'
+            )
+
+    except Exception as e:
+        logging.error(f"An error occurred during execution: {e}")
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,9 @@
 from datetime import datetime
 import csv
-import logging
+import glob as bolg
+import logging as log
 import os
+import pandas as pd
 import shutil
 import subprocess
 
@@ -11,29 +13,29 @@ def setupProjectStructure(INPUT_FILE, BACKUP_FILE, DIRECTORIES):
 
     try:
         subprocess.check_call([os.sys.executable, '-m', 'pip', 'install', '-r', 'requirements.txt'])
-        logging.info("All requirements installed successfully.")
+        log.info("All requirements installed successfully.")
 
     except subprocess.CalledProcessError as e:
-        logging.error(f"Error installing requirements: {e}")
+        log.error(f"Error installing requirements: {e}")
 
     except Exception as e:
-        logging.error(f"Unexpected error during installation: {e}")
+        log.error(f"Unexpected error during installation: {e}")
 
     if not os.path.exists(BACKUP_FILE):
-        logging.error(f"Backup file `{BACKUP_FILE}` is missing. Please clone a fresh copy of `almondhousepublishing27/website-words`.")
+        log.error(f"Backup file `{BACKUP_FILE}` is missing. Please clone a fresh copy of `almondhousepublishing27/website-words`.")
 
     for dir in DIRECTORIES:
 
         if not os.path.exists(dir):
             os.makedirs(dir)
-            logging.info(f"Created directory: {dir}")
+            log.info(f"Created directory: {dir}")
 
     if not os.path.exists(INPUT_FILE):
         shutil.copy(BACKUP_FILE, INPUT_FILE)
-        logging.info(f"Copied `{BACKUP_FILE}` to `{INPUT_FILE}`")
+        log.info(f"Copied `{BACKUP_FILE}` to `{INPUT_FILE}`")
 
     else:
-        logging.info(f"Backup file `{BACKUP_FILE}` not required.")
+        log.info(f"Backup file `{BACKUP_FILE}` not required.")
 
 ###--------------------------------->>>>>>>
 # read URLs from CSV file
@@ -42,46 +44,102 @@ def readDataInput(filename='data/input/url-list.csv'):
     url_list = {}
 
     try:
-
         with open(filename, mode='r') as file:
             reader = csv.DictReader(file)
 
             for row in reader:
                 url_list[row['Institution']] = row['Website']
 
-        logging.info(f"URLs loaded from `{filename}`")
+        log.info(f"URLs loaded from `{filename}`")
         
     except Exception as e:
-        logging.error(f"Error loading URLs from `{filename}`: {e}")
+        log.error(f"Error loading URLs from `{filename}`: {e}")
 
     return url_list
 
 ###--------------------------------->>>>>>>
 # write word count data to CSV file
-def writeDataOutput(data, filename='data/output/word-data.csv'):
+def writeWordData(data, filename='data/output/word-data.csv'):
 
     timestamp = datetime.now().strftime("%Y%m%d-%H%M")
     base, ext = os.path.splitext(filename)
     filename = f"{base}-{timestamp}{ext}"
 
     try:
-
         with open(filename, mode='w', newline='') as file:
             writer = csv.writer(file)
-            writer.writerow(['Institution', 'Word', 'Count'])
+            writer.writerow([
+                'Institution',
+                'Word',
+                'Count'
+            ])
 
             for institution, word_data in data.items():
-
                 for word, count in word_data.items():
-                    writer.writerow([institution, word, count])
+                    writer.writerow([
+                        institution,
+                        word,
+                        count
+                    ])
 
-        logging.info(f"Data saved to `{filename}`")
-
-        if os.path.exists(filename):
-            logging.info(f"File `{filename}` was successfully created.")
-
-        else:
-            logging.error(f"File `{filename}` was NOT created.")
+        log.info(f"Data saved to `{filename}`")
     
     except Exception as e:
-        logging.error(f"Error saving data to `{filename}`: {e}")
+        log.error(f"Error saving data to `{filename}`: {e}")
+
+###--------------------------------->>>>>>>
+# write site details data to CSV file
+def writeSiteData(data, filename='data/output/site-data.csv'):
+    
+    timestamp = datetime.now().strftime("%Y%m%d-%H%M")
+    base, ext = os.path.splitext(filename)
+    filename = f"{base}-{timestamp}{ext}"
+
+    try:
+        with open(filename, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([
+                'Institution',
+                'ImageCount',
+                'LinkCount',
+                'UsesJavaScript',
+                'CertificateInfo',
+                'HostInfo'])
+
+            for institution, site_data in data.items():
+                writer.writerow([
+                    institution,
+                    site_data['images'],
+                    site_data['links'],
+                    site_data['js'],
+                    site_data['cert'],
+                    site_data['host']
+                ])
+
+        log.info(f"Site data saved to `{filename}`")
+
+    except Exception as e:
+        log.error(f"Error saving site data to `{filename}`: {e}")
+
+###--------------------------------->>>>>>>
+# sorts the datafiles after writing
+
+def sortDataOutput(word_data_pattern, site_data_pattern):
+
+    word_data_files = bolg.glob(word_data_pattern)
+    latest_word_file = max(word_data_files, key=os.path.getctime) if word_data_files else None
+
+    site_data_files = bolg.glob(site_data_pattern)
+    latest_site_file = max(site_data_files, key=os.path.getctime) if site_data_files else None
+
+    if latest_word_file:
+        word_data = pd.read_csv(latest_word_file)
+        sorted_word_data = word_data.sort_values(by=['Institution', 'Word'])
+        sorted_word_data.to_csv(latest_word_file, index=False)
+        log.info(f"Sorted word data saved to `{latest_word_file}`")
+
+    if latest_site_file:
+        site_data = pd.read_csv(latest_site_file)
+        sorted_site_data = site_data.sort_values(by=['Institution'])
+        sorted_site_data.to_csv(latest_site_file, index=False)
+        log.info(f"Sorted site data saved to `{latest_site_file}`")
